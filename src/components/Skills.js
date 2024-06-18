6d1ff43091a69ea1c3449a327570a442ce2f4c68
Steven Brady <bradys@my.lanecc.edu>
2024-06-18 14:15:07 -0700

    added basic components

---
+++ src/components/Skills.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <section id="skills">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>HTML & CSS</li>
+        <li>Node.js</li>
+        <li>Python</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
