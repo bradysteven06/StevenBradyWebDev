824aed7b18186096e338ac74254fadf5dd0a2ad9
Steven Brady <bradys@my.lanecc.edu>
2024-06-22 04:22:32 -0700

    adjusted header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import '../assets/styles/Header.css'; // Corrected import path
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="#">My Portfolio</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
+    <Navbar bg="dark" variant="dark" expand="lg" className="custom-navbar">
+      <Navbar.Brand href="#" className="brand">My Portfolio</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className="order-lg-2 ml-auto" />
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center order-lg-1">
+        <Nav>
           <Nav.Link as={Link} to="/about">About</Nav.Link>
           <Nav.Link as={Link} to="/skills">Skills</Nav.Link>
           <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
